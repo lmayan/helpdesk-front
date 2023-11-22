eae16da2d8885e3f897cae4e44f1f2e0abd1a9fc
lmayan <lucasmayanae@gmail.com>
2023-11-22 15:53:59 -0300

    create NAV component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,9 +24,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+// Project Components
+import { NavComponent } from './components/nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
