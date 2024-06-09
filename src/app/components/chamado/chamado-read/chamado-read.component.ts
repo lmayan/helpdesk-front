19d49bf6584c7e54858f472997de0054aa05f003
lmayan <lucasmayanae@gmail.com>
2024-06-09 13:25:09 -0300

    finished component chamado-read

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-read',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoReadComponent implements OnInit {
 
-  constructor() { }
+  chamado: Chamado = {
+    prioridade: "",
+    status: "",
+    titulo: "",
+    observacao: "",
+    tecnico: "",
+    cliente: "",
+    nomeCliente: "",
+    nomeTecnico: "",
+  };
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private toastrService: ToastrService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get("id");
+    this.findById();
+  }
+
+  findById(){
+    this.chamadoService.findById(this.chamado.id).subscribe({
+      next: (res) => { 
+        this.chamado = res
+      },
+      error: (err) => { 
+        this.toastrService.error("Erro ao consultar chamado!", "ERRO")
+        console.error('Error occurred:', err); },
+      complete: () => {}
+    });
+  }
+
+  returnStatus(status: string){
+    if(status == '0'){
+      return 'ABERTO'
+    } else if(status == '1'){
+      return 'ANDAMENTO'
+    }else{
+      return 'ENCERRADO'
+    }
+  }
+
+  returnPrioridade(prioridade: string){
+    if(prioridade == '0'){
+      return 'BAIXA'
+    } else if(prioridade == '1'){
+      return 'MEDIA'
+    }else{
+      return 'ALTA'
+    }
   }
 
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
   ){ }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/update'])
+    this.router.navigate(['chamados/read/1'])
   }
   
   logout(){
